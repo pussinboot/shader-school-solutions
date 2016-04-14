9044a2b3f18dce8e10302d90be682301c884fb8f
Leo <leooooo@bu.edu>
2016-04-14 14:31:35 -0400

    matrix powers, end of getting started

--- 06-intro-6/mpow.glsl
+++ 06-intro-6/mpow.glsl
@@ -6,8 +6,14 @@ mat2 matrixPower(highp mat2 m, int n) {
   //
   //  matrixPower(m, 2) = m * m
   //
-
-  return mat2(1.0);  
+  highp mat2 ans = mat2(1.0);
+  for (int i = 0; i < 16; i++){
+  	if (i >= n){
+  		return ans;
+  	}
+  	ans = ans * m;
+  }
+  return ans;  
 }
 
 //Do not change this line or the name of the above function
