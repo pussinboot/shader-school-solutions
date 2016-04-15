89f745304eaf5934422ba643142e7ad52afd41df
pussinboot <>
2016-04-15 14:54:51 -0400

    done with vertex shaders

--- 11-vert-2/fragment.glsl
+++ 11-vert-2/fragment.glsl
@@ -1,5 +1,7 @@
 precision highp float;
 
+varying vec4 inColor;
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  gl_FragColor = inColor;
 }
--- 11-vert-2/vertex.glsl
+++ 11-vert-2/vertex.glsl
@@ -2,7 +2,9 @@ precision highp float;
 
 attribute vec4 position;
 attribute vec3 color;
+varying vec4 inColor;
 
 void main() {
   gl_Position = position;
+  inColor = vec4(color,1.0);
 }
