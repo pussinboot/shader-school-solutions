561baab9735478e742fe43d8a880d30535682ec8
Leo <leooooo@bu.edu>
2016-04-20 16:45:21 -0400

    geometry transform

--- 12-geom-1/transforms.glsl
+++ 12-geom-1/transforms.glsl
@@ -7,6 +7,9 @@ uniform mat4 model, view, projection;
 void main() {
 
   //TODO: Apply the model-view-projection matrix to `position`
+  // have to think about why the order matters lol
+  //gl_Position = model*view*projection*vec4(position, 1);
 
-  gl_Position = vec4(position, 1);
+  gl_Position = projection*view*model*vec4(position, 1);
+
 }
