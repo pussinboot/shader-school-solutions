d654f1a046463677e140f826e7f1ca6081f04f41
Leo <leooooo@bu.edu>
2016-04-21 13:48:44 -0400

    geometry translate

--- 12-geom-1/transforms.glsl
+++ 12-geom-1/transforms.glsl
@@ -8,7 +8,7 @@ void main() {
 
   //TODO: Apply the model-view-projection matrix to `position`
   // have to think about why the order matters lol
-  //gl_Position = model*view*projection*vec4(position, 1);
+  //gl_Position = vec4(position, 1)*model*view*projection;
 
   gl_Position = projection*view*model*vec4(position, 1);
 
