9ceeec2003110adc06e68bd1c31bc2e1967a5d4a
Leo <leooooo@bu.edu>
2016-04-27 09:14:39 -0400

    phong lighting

--- 19-light-3/fragment.glsl
+++ 19-light-3/fragment.glsl
@@ -5,6 +5,15 @@ uniform mat4 inverseModel, inverseView, inverseProjection;
 uniform vec3 ambient, diffuse, specular, lightDirection;
 uniform float shininess;
 
+varying vec3 normalVector;
+varying vec3 eyeDirection;
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+	vec3 rlight = reflect(lightDirection, normalVector);
+	float eyeLight = max(dot(rlight, eyeDirection), 0.0);
+	float phong = pow(eyeLight, shininess);
+
+	float lambert = max(dot(normalVector, lightDirection),0.0);
+	vec3 outputColor = ambient + diffuse * lambert + specular * phong;
+	gl_FragColor = vec4(outputColor,1);
 }
