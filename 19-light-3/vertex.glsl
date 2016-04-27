8aab68427225c24f5dd15f78af12862bb674ea42
pussinboot <>
2016-04-27 09:14:39 -0400

    phong lighting

--- 19-light-3/vertex.glsl
+++ 19-light-3/vertex.glsl
@@ -6,6 +6,11 @@ uniform mat4 inverseModel, inverseView, inverseProjection;
 uniform vec3 ambient, diffuse, specular, lightDirection;
 uniform float shininess;
 
+varying vec3 normalVector;
+varying vec3 eyeDirection;
+
 void main() {
-  gl_Position = vec4(position, 1);
+  gl_Position = projection*view*model*vec4(position, 1);
+  normalVector = normalize((vec4(normal,1) * inverseModel * inverseView).xyz);
+  eyeDirection = normalize((view*model*vec4(position, 1)).xyz);
 }
