1b354d6a6c9f8ed06ad8c5d61d185c4b82b3490d
pussinboot <>
2016-04-28 15:48:44 -0400

    point lights

--- 20-light-4/fragment.glsl
+++ 20-light-4/fragment.glsl
@@ -15,7 +15,17 @@ uniform vec3 specular;
 uniform vec3 lightPosition;
 
 uniform float shininess;
+varying vec3 normalVector;
+varying vec3 eyeDirection;
+varying vec3 lightDirection;
 
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+	vec3 rlight = reflect(lightDirection, normalVector);
+	float eyeLight = max(dot(rlight, eyeDirection), 0.0);
+	float phong = pow(eyeLight, shininess);
+
+	float lambert = max(dot(normalVector, lightDirection),0.0);
+	vec3 outputColor = ambient + diffuse * lambert + specular * phong;
+	gl_FragColor = vec4(outputColor,1);
+	// gl_FragColor = vec4(ambient,1);
 }
