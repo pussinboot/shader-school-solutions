3b58dc9bb38916d2ebe8a6e5fa5e4753e94b0193
pussinboot <>
2016-04-29 14:55:14 -0400

    wave equation

--- 26-gpgpu-3/wave.glsl
+++ 26-gpgpu-3/wave.glsl
@@ -2,6 +2,8 @@ precision mediump float;
 
 uniform sampler2D prevState[2];
 uniform vec2 stateSize;
+uniform float kdiffuse;
+uniform float kdamping;
 
 float state0(vec2 x) {
   return texture2D(prevState[0], fract(x / stateSize)).r;
@@ -10,12 +12,13 @@ float state1(vec2 x) {
   return texture2D(prevState[1], fract(x / stateSize)).r;
 }
 
+float laplacian(vec2 x) {
+  return (state0(x+vec2(-1,0)) + state0(x+vec2(1,0)) + state0(x+vec2(0,1)) + state0(x+vec2(0,-1))) - 4.0 * state0(x);
+}
+
 void main() {
   vec2 coord = gl_FragCoord.xy;
 
-  //TODO: Solve for next state using a 5-point Laplacian stencil and the explicit Euler rule
-
-  float y = state0(coord);
-
+  float y = (1.0 - kdamping) * (kdiffuse * laplacian(coord) + 2.0 * state0(coord)) - state1(coord);
   gl_FragColor = vec4(y,y,y,1);
 }
